4f1f8352c27c56edad1c12b3d5372197d2f39673
Martin Stahlhut <martinsta.51871@redlandsschools.net>
2018-09-12 15:26:19 +0000

    oof

--- evenOrOdd.py
+++ evenOrOdd.py
@@ -1,4 +1,11 @@
 """
 Write program that will take a number (Integers only) and
-return a statement that will let 
+return a statement that will let the user know if it is even or odd
 """
+x = int(input("To determine if even or odd, insert INTEGER here: "))
+if x % 2 == 0:
+    print(x, "is an even number.")
+elif x % 2 == 1:
+    print(x, "is an odd number.")
+else:
+    print("Cmon man you didn't put in an INTERGER...")
