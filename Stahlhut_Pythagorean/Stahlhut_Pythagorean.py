fd6f6066d58400c7d3de448fbe902a7575fe0f6f
Martin Stahlhut <martinsta.51871@redlandsschools.net>
2018-09-14 15:25:37 +0000

    Stahlhut_Pythagorean

--- Stahlhut_Pythagorean/Stahlhut_Pythagorean.py
+++ Stahlhut_Pythagorean/Stahlhut_Pythagorean.py
@@ -1,1 +1,5 @@
-"""
+name = input('Welcome to the python pythagorean calculator, what is your name? ')
+print("Well welcome, " + name + " to the power of PYTHON and POSSIBILITY...")
+
+a = input("So, " + name + " what would you like to input as your first integer... ")
+b = input("Okay almost done, what would you like to use as your second integer... ")
