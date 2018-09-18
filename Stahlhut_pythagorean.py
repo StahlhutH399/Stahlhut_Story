85e3c1f7406ad96167268a5e8d892c300027622c
Martin Stahlhut <martinsta.51871@redlandsschools.net>
2018-09-18 16:16:08 +0000

    Adding

---
+++ Stahlhut_pythagorean.py
@@ -0,0 +1,7 @@
+import math
+name = input('Welcome to the python pythagorean calculator, what is your name? ')
+print("Well welcome, " + name + " to the power of PYTHON and POSSIBILITY...")
+
+a = str(input("So, " + name + " what would you like to input as your first integer... "))
+b = str(input("Okay almost done, what would you like to use as your second integer... "))
+print("The hypotenuse of your triangle is " + a**2+b**2 + "!")
---
+++ amazing.py
@@ -0,0 +1,17 @@
+"""
+This prgram demonstrates IMPORT statement and LOOPS
+"""
+
+# import statements go FIRST! (TOP OF YOUR CODE)
+import time
+
+#for variableName in range(start, stop, step):
+# OR
+# for variableName in range(stop): *This will start at 0 and step by 1
+
+for Thoe in range (20, -6, -2):
+    print('Hello ' + str(Thoe))
+    
+    
+# Count to 100 by 5's
+# Count from (START AT 20) by 2's and stop at -4
